73d43f297cc878f1b19ed94d238e3b97365ead50
thomp93 <159131227+thomp93@users.noreply.github.com>
2024-05-30 09:11:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,45 @@
-import streamlit
+import streamlit as st
 import snowflake.connector
-import pandas
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
-# connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+import pandas as pd
+
+st.title("Zena's Amazing Athleisure Catalog")
+
+# Connect to Snowflake
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
+
+# Run a Snowflake query and put it all in a variable called my_catalog
+my_cur.execute("SELECT color_or_style FROM catalog_for_website")
 my_catalog = my_cur.fetchall()
-# put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
-# temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
-# put the first column into a list
-color_list = df[0].values.tolist()
+
+# Put the data into a DataFrame
+df = pd.DataFrame(my_catalog, columns=["color_or_style"])
+
+# Temp write the DataFrame to the page so I can see what I am working with
+# st.write(df)
+
+# Put the first column into a list
+color_list = df["color_or_style"].values.tolist()
+
+# Print color_list (for debugging)
 # print(color_list)
+
 # Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option = st.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-# use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where
-color_or_style = '" + option + "';")
+
+# Use the option selected to go back and get all the info from the database
+my_cur.execute(f"SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website WHERE color_or_style = '{option}'")
 df2 = my_cur.fetchone()
-streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
+
+# Display the image and product information
+st.image(
+    df2[0],
+    width=400,
+    caption=product_caption
 )
-streamlit.write('Price: ', df2[1])
-streamlit.write('Sizes Available: ',df2[2])
-streamlit.write(df2[3])
+st.write('Price: ', df2[1])
+st.write('Sizes Available: ', df2[2])
+st.write(df2[3])
